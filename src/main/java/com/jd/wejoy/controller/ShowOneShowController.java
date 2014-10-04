3705590697099a0779a896a72caacb2a049d0c4e
zhulang <zhulang@jd.com>
2014-10-05 07:59:45 +0800

    update
    Signed-off-by: zhaoyonggang <zhaoyonggang@jd.com>

---
+++ src/main/java/com/jd/wejoy/controller/ShowOneShowController.java
@@ -0,0 +1,24 @@
+package com.jd.wejoy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * 版权信息：@copyright Copyright 2014 JD.COM All Right Reserved
+ * 关联类: ( @see )
+ * 作者： zhaoyonggang 部门：云平台 |开放云事业部 |开放研发组 
+ * 版本： 1.0
+ * Date: 2014-10-5
+ */
+
+@Controller
+public class ShowOneShowController {
+	@RequestMapping(value="/shopcicle",  method = {RequestMethod.GET, RequestMethod.POST})
+	public String shopcicle(ModelMap model) {
+		return "shopcicle";
+	}
+	
+}
